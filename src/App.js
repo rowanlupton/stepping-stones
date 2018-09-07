4d9db88f14a1d5952dfa7c17dae203903179b1e1
rowan <rowan@rowan.website>
2018-09-07 03:16:07 -0400

    Add settings page to control board-draw
    and, like, a bunch of other stuff. more granular commits coming from 
    here on out

--- src/App.js
+++ src/App.js
@@ -1,61 +1,152 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const NAME = 'Rowan';
-
 function Box (props) {
-    return <div className="Box">
-        <div className="score">0</div>
-        <div className="bid">null</div>
-        <div className="tricks-taken">null</div>
-        <div className="difference">null</div>
-    </div>;
+    return  <div className="Box">
+              <div className="score">{props.score}</div>
+              <div className="bid">{props.bid}</div>
+              <div className="tricks-taken">{props.tricksTaken}</div>
+              <div className="difference">{props.difference}</div>
+            </div>;
 }
 Box.defaultProps = {
-    what: 'box'
+    score: 0,
+    bid: null,
+    tricksTaken: null,
+    difference: null
 }
 
 function Row (props) {
-    return <div className="Row">
-        <div className="round-information">
-            <div className="dealer">Dealer</div>
-        <div className="trump" data-trump="heart">&#9829;</div>
-        <div className="cards-number">{props.cardsNumber}</div>        
-        </div>
-        
-        <Box />
-        <Box />
-        <Box />
-    </div>;
+  const {dealer,trump,cardsNumber,numberOfPlayers} = props
+  return  <div className="Row">
+            <div className="round-information">
+              <div className="dealer">{dealer}</div>
+              <div className="trump">{trump}</div>
+              <div className="cards-number">{cardsNumber}</div>
+            </div>
+
+            {[...Array(numberOfPlayers)].map((player,i) =>
+              <Box key={i} />
+            )}
+          </div>
+}
+Row.defaultProps = {
+  dealer: null,
+  trump: null,
+  cardsNumber: null
+}
+
+class Board extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {...props}
+
+    let iterator = [...Array(this.state.startingNumber)];
+    let roundNumbers = [];
+    for (let i = iterator.length; i >= 2; i--) {
+        roundNumbers.push(i);
+    }
+
+    this.state.roundNumbers = roundNumbers.concat([...roundNumbers].reverse());
+    if (this.state.toggleRoundOfOne) {
+      this.state.roundNumbers.splice(this.state.startingNumber - 1,0,1)
+    }
+  }
+
+  render () {
+    const {roundNumbers, numberOfPlayers} = this.state
+    return (
+      <div id="board">
+        {roundNumbers.map((x,i) =>
+          <Row key={i} rowKey={i} cardsNumber={x} numberOfPlayers={numberOfPlayers} />
+        )}
+        hi
+      </div>
+    )
+  }
+}
+
+class Settings extends Component {
+  constructor(props) {
+    super(props)
+    // this.state = {...props}
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange (event) {
+    this.props.updateSettings(event)
+  }
+  handleSubmit (event) {
+    event.preventDefault()
+    this.props.applySettings(event)
+  }
+  render () {
+    const {
+      numberOfPlayers,
+      startingNumber,
+      toggleRoundOfOne
+    } = this.props
+    return  <form onSubmit={this.handleSubmit} className="Settings">
+              <label>Number of Players:
+                <input type="number" name="numberOfPlayers" onChange={this.handleChange} value={numberOfPlayers} />
+              </label>
+              <label>Starting number:
+                <input type="number" name="startingNumber" onChange={this.handleChange} value={startingNumber} />
+              </label>
+              <label> Play the round of one?
+                <input type="checkbox" name="toggleRoundOfOne" onChange={this.handleChange} value={toggleRoundOfOne} />
+              </label>
+              <label> Start
+                <input type="submit" />
+              </label>
+            </form>
+          }
 }
 
 class App extends Component {
-    render() {
-        const startingNumber = 7;
-        let iterator = [...Array(startingNumber)];
-        let roundNumbers = [];
-        for (let i = iterator.length; i >= 2; i--) {
-            roundNumbers.push(i);
-        }
-        roundNumbers = roundNumbers.concat([...roundNumbers].reverse());
-        // don't add the round of one
-        //roundNumbers.splice(startingNumber-1,0,1);        
-        
+  constructor (props) {
+    super(props);
+    this.state = {
+      settings: false,
+      numberOfPlayers: 4,
+      startingNumber: 7,
+      toggleRoundOfOne: false,
+      players: []
+    }
+    this.updateSettings = this.updateSettings.bind(this)
+    this.applySettings = this.applySettings.bind(this)
+  }
+  updateSettings (event) {
+    const t = event.target
+    // not using the ~~fancy~~ syntax here for consistency with `value`
+    const name = t.name
+    const value = t.type === 'checkbox' ? t.checked : Number(t.value)
+
+    this.setState({[name]: value});
+  }
+  applySettings (event) {
+    this.setState((settings) => ({settings: true}))
+  }
+
+  render () {
+    const {settings} = this.state
     return (
       <div className="App">
         <header className="App-header">
             <h1 className="App-title">Stepping Stones Scoring</h1>
         </header>
-        <p className="App-intro">
-            hi there, {NAME}
-        </p>
-        <div id="board">
-            {roundNumbers.map((x,i) =>
-                                <Row key={i} cardsNumber={x} />
-                               )}
-        </div>
+        {!settings ? (
+          <Settings
+            {...this.state}
+            updateSettings={this.updateSettings}
+            applySettings={this.applySettings}/>
+        ) : (
+          <Board {...this.state}/>
+        ) }
       </div>
-    );
+    )
   }
 }
 
